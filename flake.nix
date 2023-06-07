2e22da85ab4378befc743db68186147a003cad07
Kemal Maolana <128290445+kemalmao19@users.noreply.github.com>
2023-06-07 20:32:43 +0700

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -1,38 +1,30 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-
     home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = {self, home-manager, ...}@inputs:
+  outputs = { self, home-manager, ... } @ inputs:
     let
       system = "x86_64-linux";
-      # https://search.nixos.org/ <-- mencari packages apa saja yang
-      # tersedia di nix;
       pkgs = inputs.nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations = {
         kemal = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-
           modules = [
-            ({config, lig, pkgs, ...}:{
+            ({ config, lig, pkgs, ... }:{
               home.stateVersion = "22.11";
               home.username = "kemal";
               home.homeDirectory = "/home/kemal";
-              #allowUnfree = true;
-	      
+              # allowUnfree = true;
               imports = [
-                ./nixpkgs/packages.nix
-                ./nixpkgs/shell.nix
-                ./nixpkgs/git.nix
-                ./nixpkgs/terminal.nix
-		##./nixpkgs/config.nix
-  ];
-		})
+                ./nixpkgs/home.nix
+              ];
+            })
           ];
         };
       };
     };
 }
+
