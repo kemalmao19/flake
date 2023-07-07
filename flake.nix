c80df04025ab028d0e094e9eaee390f31d86547f
Kemal Maolana <128290445+kemalmao19@users.noreply.github.com>
2023-07-07 11:02:50 +0700

    new channel 23.05

--- flake.nix
+++ flake.nix
@@ -1,30 +1,24 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, home-manager, ... } @ inputs:
+  outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations = {
         kemal = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
-            ({ config, lig, pkgs, ... }:{
-              home.stateVersion = "22.11";
-              home.username = "kemal";
-              home.homeDirectory = "/home/kemal";
-              # allowUnfree = true;
-              imports = [
-                ./nixpkgs/home.nix
-              ];
-            })
+            ({ config, lib, pkgs, ... }: { imports = [ ./nixpkgs/home.nix ]; })
           ];
         };
       };
     };
 }
-
