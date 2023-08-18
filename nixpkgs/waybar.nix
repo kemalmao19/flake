a86a93546e403cb832ea567ba6c46d5991dd0478
kemalmao19 <sebelumtidur14@gmail.com>
2023-08-18 12:51:34 +0700

    add waybar

---
+++ nixpkgs/waybar.nix
@@ -0,0 +1,29 @@
+{ pkgs, config, ... }:
+
+{
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 30;
+        output = [ "eDP-1" "HDMI-A-1" ];
+        modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
+        modules-center = [ "sway/window" "custom/hello-from-waybar" ];
+        modules-right = [ "mpd" "custom/mymodule#with-css-id" "temperature" ];
+
+        "sway/workspaces" = {
+          disable-scroll = true;
+          all-outputs = true;
+        };
+        "custom/hello-from-waybar" = {
+          format = "hello {}";
+          max-length = 40;
+          interval = "once";
+          exec = pkgs.writeShellScript "hello-from-waybar" ''
+            echo "from within waybar"
+          '';
+        };  
+     };
+}
