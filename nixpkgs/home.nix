7e1d5dd9e8337ec2eed0ac16bead5088c15fcbd3
kemalmao19 <sebelumtidur14@gmail.com>
2023-09-26 23:36:14 +0700

    update pantheon

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -8,19 +8,7 @@
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
 
-  # gtk = {
-  #   enable = true;
-  #   theme = {
-  #     name = "Catppuccin-Macchiato-Compact-Pink-dark";
-  #     package = pkgs.catppuccin-gtk.override {
-  #       accents = [ "pink" ];
-  #       size = "compact";
-  #       tweaks = [ "rimless" ];
-  #       variant = "macchiato";
-  #     };
-  #   };
-  # };
-
+  
   imports = [
     ./packages.nix
     ./shell.nix
@@ -28,7 +16,7 @@
     # ./alacritty.nix
     ./starship_symbol.nix
     ./git.nix
-    ./gtk.nix
+    # ./gtk.nix
     ./python.nix
     # ./config.nix
   ];
