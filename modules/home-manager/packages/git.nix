4ed322deda3e867d0c56cf00f8a4d85b17f447f1
kemalmao19 <sebelumtidur14@gmail.com>
2024-10-18 16:28:14 +0700

    git auth gh git

--- modules/home-manager/packages/git.nix
+++ modules/home-manager/packages/git.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }: {
-
-  programs.gh.gitCredentialHelper = { enable = true; };
   programs.git = {
     enable = true;
     userName = "kemalmao19";
     userEmail = "sebelumtidur14@gmail.com";
 
   };
+
+  programs.gh.gitCredentialHelper.enable = true;
 }
