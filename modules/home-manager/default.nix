c8cff479e00021067b3f5c963018a0fccef2d504
kemalmao19 <sebelumtidur14@gmail.com>
2025-04-23 04:40:16 +0700

    add

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -16,4 +16,10 @@
 
   home.packages = with pkgs; [ ibus-engines.mozc ];
 
+  # Chrome/Fyde OS registering
+  xdg.configFile."systemd/user/cros-garcon.service.d/override.conf".text = ''
+    [Service]
+    Environment="PATH=%h/.nix-profile/bin:/usr/local/sbin:/usr/local/bin:/usr/local/games:/usr/sbin:/usr/bin:/usr/games:/sbin:/bin"
+    Environment="XDG_DATA_DIRS=%h/.nix-profile/share:%h/.local/share:%h/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share"
+  '';
 }
